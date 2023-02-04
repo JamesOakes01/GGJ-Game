45c108bfd935315e0897cc8663a9c2993feaedc4
EthanTFH <egibson8202@gmail.com>
2023-02-04 14:25:11 -0600

    Large Additions
    + Basic Menu
    + Changed Player Collider
    + Changed Player Handler
    + Created Player Class
    + Added Menu Audio

--- GGJ-Game/Assets/Scripts/PlayerController.cs
+++ GGJ-Game/Assets/Scripts/PlayerController.cs
@@ -1,37 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Player Defaults")]
+
+    // Spawn Location
     [InspectorName("Spawn Location")]
     public Vector2 spawnLocation = Vector2.zero;
-    [InspectorName("Player Speed")]
+    
+    // Players walking speed
+    [InspectorName("Walk Speed")]
     public float speed = 0.5f;
+
+    // Players run speed
     [InspectorName("Run Speed")]
     public float runSpeed = 0.7f;
 
+    // Sprite list
     [Header("Sprites")]
     public Sprite[] sprites;
 
+    // (DEFAULT) Zoom Sensitivity
     [Header("Game Settings")]
-    [InspectorName("Zoom Sensetivity")]
+    [InspectorName("Zoom Sensitivity")]
     public float zoomSense = 5;
 
-    private Sprite currentSprite;
+    // Minimum Zoom
+    [InspectorName("Minimum Zoom")]
+    public float minFOV = 50;
 
-    // Start is called before the first frame update
+    // Maximum Zoom
+    [InspectorName("Maximum Zoom")]
+    public float maxFOV = 100;
+
+    // (HIDDEN) Player Object
+    [HideInInspector]
+    public Player player;
+
     void Start()
     {
-        this.gameObject.transform.position = spawnLocation;
-        currentSprite = sprites[0];
-        this.GetComponent<SpriteRenderer>().sprite = currentSprite;
+        // If GlobalSettings set
+        if(GameObject.Find("GlobalSettings"))
+        {
+            GameObject globalSettings = GameObject.Find("GlobalSettings");
+            this.zoomSense = (float)Variables.Object(globalSettings).Get("ZoomSensitivity");
+        }
+
+        // Create Player
+        player = new Player(this.gameObject, sprites[0]);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Handle Input
         if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
             float xAxis = Input.GetAxis("Horizontal");
@@ -48,19 +72,92 @@ public class PlayerController : MonoBehaviour
                 yAxis *= speed;
             }
 
-            Vector2 currenPost = new Vector2(this.transform.position.x, this.transform.position.y);
-            this.GetComponent<Rigidbody2D>().MovePosition(currenPost + new Vector2(xAxis*speed, yAxis*speed));
+            player.move(new Vector2(this.transform.position.x + xAxis, this.transform.position.y + yAxis));
         }
 
+        // Handle Camera Zooming
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
             // Camera Zoom Magic
             Camera cam = gameObject.GetComponentInChildren<Camera>();
             cam.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoomSense;
-            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 50, 90);
+            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, minFOV, maxFOV);
+        }
+    }
+
+
+
+
+
+    /*
+     * 
+     * Player Class
+     * 
+     * I don't know why I did this, but I did
+     * It works amazingly and lets me access the player without cluttering the rest of the code.
+     * 
+     */
+    public class Player
+    {
+        private GameObject parent;
+        private Rigidbody2D body;
+        private Camera cam;
+        private SpriteRenderer renderer;
+
+        private Vector2 moveTowards;
+
+        public Player(GameObject parent, Sprite initialSprite)
+        {
+            this.parent = parent;
+            this.body = parent.GetComponent<Rigidbody2D>();
+            this.cam = parent.GetComponentInChildren<Camera>();
+            this.renderer = parent.GetComponent<SpriteRenderer>();
+
+            if (!parent || !body || !cam) throw new MissingComponentException("Player Not Setup Correctly.");
+            if (!initialSprite) throw new MissingComponentException("Player must be provided an initial sprite.");
+            renderer.sprite = initialSprite;
         }
 
-        if(this.GetComponent<SpriteRenderer>().sprite != currentSprite)
-            this.GetComponent<SpriteRenderer>().sprite = currentSprite;
+        public void move(Vector2 moveTowards)
+        {
+            if (!checkWriteAccess(System.Environment.StackTrace)) throw new System.Exception("Illegal Access to Player.move()\nOnly A PlayerController may use this.");
+            body.MovePosition(moveTowards);
+        }
+
+        public void setPosition(Vector2 pos)
+        {
+            if (!checkWriteAccess(System.Environment.StackTrace)) throw new System.Exception("Illegal Access to Player.setPosition()\nOnly A PlayerController may use this.");
+            parent.transform.position = pos;
+        }
+
+        public void setSprite(Sprite sprite)
+        {
+            if (!checkWriteAccess(System.Environment.StackTrace)) throw new System.Exception("Illegal Access to Player.setSprite()\nOnly A PlayerController may use this.");
+            parent.GetComponent<SpriteRenderer>().sprite = sprite;
+        }
+
+        public Sprite getSprite()
+        {
+            return this.renderer.sprite;
+        }
+
+        public Vector2 getPosition()
+        {
+            return this.parent.transform.position;
+        }
+
+        /*
+         * Prevent Anything other than authorized controllers from setting things.
+         */
+        private bool checkWriteAccess(string stackTrace)
+        {
+            if (stackTrace.Split("\n")[2].Contains("PlayerController.Update () "))
+            {
+                //return true;
+            }
+
+            return false;
+        }
+
     }
 }
